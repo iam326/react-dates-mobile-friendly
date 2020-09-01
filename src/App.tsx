a39ebfebcfcd5114784e86692d99030a0037ef1c
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-09-02 01:07:01 +0900

    add DateRangePicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
+
+import 'moment/locale/ja';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+import { DateRangePicker } from 'react-dates';
+import moment from 'moment';
 
 function App() {
-  return <div>hello world</div>;
+  const [startDate, setStartDate] = useState<moment.Moment | null>(null);
+  const [endDate, setEndDate] = useState<moment.Moment | null>(null);
+  const [focusedInput, setFocusedInput] = useState<
+    'startDate' | 'endDate' | null
+  >(null);
+  return (
+    <div>
+      <DateRangePicker
+        startDate={startDate}
+        startDateId="startDateId"
+        endDate={endDate}
+        endDateId="endDateId"
+        onDatesChange={(selectedDates) => {
+          setStartDate(selectedDates.startDate);
+          setEndDate(selectedDates.endDate);
+        }}
+        focusedInput={focusedInput}
+        onFocusChange={setFocusedInput}
+        displayFormat="YYYY-MM-DD"
+        noBorder={false}
+        isOutsideRange={() => false}
+        hideKeyboardShortcutsPanel={true}
+        orientation="vertical"
+        withPortal={true}
+      />
+    </div>
+  );
 }
 
 export default App;
