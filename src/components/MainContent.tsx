c8d4bd13522c3fd7172418998358ed307c09a35f
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-09-03 00:20:52 +0900

    add margin

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 import DateRangePickerA from './DateRangePickerA';
 import DateRangePickerB from './DateRangePickerB';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      marginTop: theme.spacing(3),
+    },
+  })
+);
+
 const MainContent: React.FC = () => {
+  const classes = useStyles();
+
   return (
     <>
       <div>
         <DateRangePickerA />
       </div>
-      <div>
+      <div className={classes.margin}>
         <DateRangePickerB />
       </div>
     </>
