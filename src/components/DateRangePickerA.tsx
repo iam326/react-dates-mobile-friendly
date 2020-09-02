fe41deff9f1db626a8092bbdb05944ad22303cfa
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-09-03 00:08:15 +0900

    add close btn to date picker controller

--- src/components/DateRangePickerA.tsx
+++ src/components/DateRangePickerA.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import ClearIcon from '@material-ui/icons/Clear';
 import moment from 'moment';
@@ -11,13 +11,15 @@ import 'react-dates/lib/css/_datepicker.css';
 
 import './style/react-dates-custom.css';
 
-const useStyles = makeStyles(() => ({
-  close: {
-    position: 'absolute',
-    top: '5px',
-    right: '5px',
-  },
-}));
+const useStyles = makeStyles(() =>
+  createStyles({
+    close: {
+      position: 'absolute',
+      top: '5px',
+      right: '5px',
+    },
+  })
+);
 
 const DateRangePickerA: React.FC = () => {
   const classes = useStyles();
@@ -32,21 +34,21 @@ const DateRangePickerA: React.FC = () => {
       startDateId="startDateId"
       endDate={endDate}
       endDateId="endDateId"
-      onDatesChange={(selectedDates) => {
-        setStartDate(selectedDates.startDate);
-        setEndDate(selectedDates.endDate);
-      }}
       focusedInput={focusedInput}
-      onFocusChange={setFocusedInput}
       isOutsideRange={() => false}
-      hideKeyboardShortcutsPanel={true}
-      orientation="vertical"
       withPortal={true}
+      orientation="vertical"
+      hideKeyboardShortcutsPanel={true}
       renderCalendarInfo={() => (
         <IconButton aria-label="close" className={classes.close}>
           <ClearIcon />
         </IconButton>
       )}
+      onFocusChange={setFocusedInput}
+      onDatesChange={(selectedDates) => {
+        setStartDate(selectedDates.startDate);
+        setEndDate(selectedDates.endDate);
+      }}
     />
   );
 };
--- src/components/DateRangePickerB.tsx
+++ src/components/DateRangePickerB.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import ClearIcon from '@material-ui/icons/Clear';
+import moment from 'moment';
+import { DayPickerRangeController } from 'react-dates';
 
 import 'moment/locale/ja';
 import 'react-dates/initialize';
@@ -8,23 +12,22 @@ import 'react-dates/lib/css/_datepicker.css';
 
 import './style/react-dates-custom.css';
 
-import { DayPickerRangeController } from 'react-dates';
-import moment from 'moment';
-
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
-    blank: {
-      marginTop: '30px',
-    },
     textField: {
       width: '25ch',
+    },
+    close: {
+      position: 'absolute',
+      top: '5px',
+      right: '5px',
     },
   })
 );
 
 const DateRangePickerB: React.FC = () => {
   const classes = useStyles();
-  const dateFormat = 'YYYY-MM-DD';
+  const dateFormat = 'YYYY/MM/DD';
   const [startDate, setStartDate] = useState<moment.Moment | null>(null);
   const [endDate, setEndDate] = useState<moment.Moment | null>(null);
   const [focusedInput, setFocusedInput] = useState<'startDate' | 'endDate'>(
@@ -45,6 +48,23 @@ const DateRangePickerB: React.FC = () => {
       />
       {display && (
         <DayPickerRangeController
+          startDate={startDate}
+          endDate={endDate}
+          focusedInput={focusedInput}
+          numberOfMonths={2}
+          minimumNights={0}
+          withPortal={true}
+          orientation="vertical"
+          hideKeyboardShortcutsPanel={true}
+          renderCalendarInfo={() => (
+            <IconButton
+              aria-label="close"
+              className={classes.close}
+              onClick={() => setDisplay(false)}
+            >
+              <ClearIcon />
+            </IconButton>
+          )}
           onFocusChange={(focusedInput) => {
             setFocusedInput(!focusedInput ? 'startDate' : focusedInput);
           }}
@@ -56,14 +76,6 @@ const DateRangePickerB: React.FC = () => {
               setDisplay(false);
             }
           }}
-          focusedInput={focusedInput}
-          startDate={startDate}
-          endDate={endDate}
-          hideKeyboardShortcutsPanel={true}
-          numberOfMonths={2}
-          minimumNights={0}
-          withPortal={true}
-          orientation="vertical"
         />
       )}
     </>
